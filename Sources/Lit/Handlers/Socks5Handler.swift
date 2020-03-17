7d4a980dafda8f2f17f2bac1fa7cdbfa554652c2
zhuhaow <zhuhaow@gmail.com>
2020-03-17 19:22:24 +0800

    Pair socks5 handler to Connector

--- Sources/Lit/Handlers/Socks5Handler.swift
+++ Sources/Lit/Handlers/Socks5Handler.swift
@@ -2,9 +2,9 @@ import NIO
 
 public final class Socks5Handler: ChannelDuplexHandler, RemovableChannelHandler {
     public typealias InboundIn = ByteBuffer
-    public typealias InboundOut = ProxyRequest<ByteBuffer>
+    public typealias InboundOut = Never
 
-    public typealias OutboundIn = ByteBuffer
+    public typealias OutboundIn = Never
     public typealias OutboundOut = ByteBuffer
 
     enum AddressType {
@@ -12,24 +12,40 @@ public final class Socks5Handler: ChannelDuplexHandler, RemovableChannelHandler 
     }
 
     enum Socks5HandlerStatus {
-        case readingVersionAndMethods,
-            readingConnectHeader,
-            waitingConnection(AddressType)
+        case readingVersionAndMethods
+        case readingConnectHeader
+        case waitingConnection(AddressType)
     }
 
     public enum Socks5HandlerError: Error {
-        case unsupportedVersion,
-            noAuthMethodSpecified,
-            tooManyMethods,
-            noSupportedMethod,
-            unSupportedCommand,
-            protocolError,
-            invalidDomainLength,
-            invalidAddressType
+        case unsupportedVersion
+        case noAuthMethodSpecified
+        case tooManyMethods
+        case noSupportedMethod
+        case unSupportedCommand
+        case protocolError
+        case invalidDomainLength
+        case invalidAddressType
     }
 
+    enum Socks5ResponseType: UInt8 {
+        case succeeded
+        case generalFailue
+        case connectionNotAllowed
+        case networkUnreachable
+        case connectionRefused
+        case ttlExpired
+        case commandNotSupported
+        case addressTypeNotSupported
+    }
+
+    let connector: Connector
     var status: Socks5HandlerStatus = .readingVersionAndMethods
     var cache: ByteBuffer?
+
+    init(connector: Connector) {
+        self.connector = connector
+    }
 
     public func channelRead(context: ChannelHandlerContext, data: NIOAny) {
         var buffer = unwrapInboundIn(data)
@@ -104,6 +120,7 @@ public final class Socks5Handler: ChannelDuplexHandler, RemovableChannelHandler 
             return
         }
 
+        let future: EventLoopFuture<Channel>
         switch buffer.readInteger(as: UInt8.self) {
         case 1:
             guard buffer.readableBytes == 6 else {
@@ -117,7 +134,7 @@ public final class Socks5Handler: ChannelDuplexHandler, RemovableChannelHandler 
                 (4, $0.load(as: in_addr.self))
             }
             addr.sin_port = buffer.readInteger(endianness: .big, as: UInt16.self)!
-            context.fireChannelRead(wrapInboundOut(.endpoint(.address(.init(addr, host: "")))))
+            future = connector.connect(on: context.eventLoop, to: .init(addr, host: ""))
             status = .waitingConnection(.ipv4)
         case 3:
             guard let domainLength = buffer.readInteger(as: UInt8.self), domainLength > 0 else {
@@ -135,9 +152,9 @@ public final class Socks5Handler: ChannelDuplexHandler, RemovableChannelHandler 
                 }
             }
 
-            let domain = buffer.readString(length: Int(domainLength))!
+            let host = buffer.readString(length: Int(domainLength))!
             let port = buffer.readInteger(endianness: .big, as: UInt16.self)!
-            context.fireChannelRead(wrapInboundOut(.endpoint(.domain(domain, Int(port)))))
+            future = connector.connect(on: context.eventLoop, host: host, port: Int(port))
             status = .waitingConnection(.domain)
         case 4:
             var addr = sockaddr_in6()
@@ -146,11 +163,30 @@ public final class Socks5Handler: ChannelDuplexHandler, RemovableChannelHandler 
                 (16, $0.load(as: in6_addr.self))
             }
             addr.sin6_port = buffer.readInteger(endianness: .big, as: UInt16.self)!
-            context.fireChannelRead(wrapInboundOut(.endpoint(.address(.init(addr, host: "")))))
+            future = connector.connect(on: context.eventLoop, to: .init(addr, host: ""))
             status = .waitingConnection(.ipv6)
         default:
             context.fireErrorCaught(Socks5HandlerError.invalidAddressType)
             return
+        }
+
+        future.whenComplete { result in
+            switch result {
+            case let .success(channel):
+                guard case let .waitingConnection(type) = self.status else {
+                    return
+                }
+
+                let buffer = self.build(response: .succeeded, for: type, with: context.channel.allocator)
+                context.writeAndFlush(self.wrapOutboundOut(buffer), promise: nil)
+
+                let (localGlue, peerGlue) = GlueHandler.matchedPair()
+                context.channel.pipeline.addHandler(localGlue).and(channel.pipeline.addHandler(peerGlue)).whenComplete { _ in
+                    context.pipeline.removeHandler(self, promise: nil)
+                }
+            case let .failure(error):
+                context.fireErrorCaught(error)
+            }
         }
     }
 
@@ -168,5 +204,35 @@ public final class Socks5Handler: ChannelDuplexHandler, RemovableChannelHandler 
     private func cacheBuffer(buffer: inout ByteBuffer) {
         buffer.moveReaderIndex(to: 0)
         cache = buffer
+    }
+
+    private func build(response: Socks5ResponseType, for type: AddressType, with allocator: ByteBufferAllocator) -> ByteBuffer {
+        guard case let .waitingConnection(type) = status else {
+            preconditionFailure()
+        }
+
+        let length: Int
+        let typeValue: UInt8
+        switch type {
+        case .ipv4, .domain:
+            length = 4
+            typeValue = 1
+        case .ipv6:
+            length = 16
+            typeValue = 4
+        }
+
+        var buffer = allocator.buffer(capacity: 6 + length)
+
+        buffer.writeInteger(5 as UInt8)
+        buffer.writeInteger(response.rawValue)
+        buffer.writeInteger(0 as UInt8)
+        buffer.writeInteger(typeValue)
+        while buffer.writableBytes > 0 {
+            // TODO: make it faster.
+            buffer.writeInteger(0 as UInt8)
+        }
+
+        return buffer
     }
 }
