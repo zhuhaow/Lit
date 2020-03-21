e4a899d2e19f0ea0d1d5d49655448426ef2275f3
zhuhaow <zhuhaow@gmail.com>
2020-03-21 21:48:31 +0800

    Add TcpConnector

--- Sources/Lit/Connectors/TcpConnector.swift
+++ Sources/Lit/Connectors/TcpConnector.swift
@@ -1,3 +1,15 @@
-//
+import NIO
 
-import Foundation
+class TcpConnector: Connector {
+    func connect(on eventLoop: EventLoop, to address: SocketAddress) -> EventLoopFuture<Channel> {
+        buildBootstrap(on: eventLoop).connect(to: address)
+    }
+
+    func connect(on eventLoop: EventLoop, host: String, port: Int) -> EventLoopFuture<Channel> {
+        buildBootstrap(on: eventLoop).connect(host: host, port: port)
+    }
+
+    private func buildBootstrap(on eventLoop: EventLoop) -> ClientBootstrap {
+        ClientBootstrap(group: eventLoop)
+    }
+}
