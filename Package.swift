a6a183ad5e0389209f05153de7e883117ec2bce1
zhuhaow <zhuhaow@gmail.com>
2020-03-21 21:47:24 +0800

    Remove unused package

--- Package.swift
+++ Package.swift
@@ -13,7 +13,6 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
-        .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.0.0"),
     ],
     targets: [
         .target(
