951af40c6bb7b901a1533de0375a561f026687f9
zhuhaow <zhuhaow@gmail.com>
2020-04-18 19:24:01 +0800

    Add helper methods to get information from Endpoint

--- Sources/Lit/Connectors/Connector.swift
+++ Sources/Lit/Connectors/Connector.swift
@@ -5,6 +5,26 @@ public enum Endpoint {
     case address(SocketAddress)
 }
 
+extension Endpoint {
+    public var host: String {
+        switch self {
+        case .domain(let d, _):
+            return d
+        case .address(let addr):
+            return addr.ipAddress!
+        }
+    }
+    
+    public var port: Int {
+        switch self {
+        case .domain(_, let p):
+            return p
+        case .address(let addr):
+            return addr.port!
+        }
+    }
+}
+
 public protocol Connector {
     func connect(on eventLoop: EventLoop, endpoint: Endpoint) -> EventLoopFuture<Channel>
 }
